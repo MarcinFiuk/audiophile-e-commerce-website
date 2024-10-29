ecc65aad129b8f2d80f40d994aa97fa0ac088b56
Marcin Fiuk <marcinfiuk@Marcins-Laptop.local>
2024-10-29 20:54:24 +0000

    work in progress

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-  return (
-    <footer className="bg-white px-5">
-      <div className="max-w-wrapper bg-red-400">footer</div>
-    </footer>
-  );
+    return (
+        <footer className='bg-charcoal px-5'>
+            <div className='  max-w-wrapper bg-[e9e9e9]'>footer</div>
+        </footer>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      maxWidth: {
-        wrapper: "69.375rem",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+              'orange-600': 'hsl(22, 65, 57)',
+              'peach': 'hsl(21, 94, 75)',
+              'charcoal': 'hsl(0, 0, 6)',
+              'light-gray': 'hsl(0, 0, 95)',
+              'off-white': 'hsl(0, 0, 98)',
+            },
+            maxWidth: {
+                wrapper: '69.375rem',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
